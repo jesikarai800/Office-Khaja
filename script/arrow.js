d97ad7f608427fbbccea92b69e91086d275be538
jesikarai800 <jesikarai800@gmail.com>
2025-11-09 12:08:55 +0545

    first commit

--- script/arrow.js
+++ script/arrow.js
@@ -1,5 +1,5 @@
 const container = document.getElementById("menu-container");
-const scrollAmount = 300; // adjust how much to scroll
+const scrollAmount = 400; // adjust how much to scroll
 
 document.getElementById("scroll-left").addEventListener("click", () => {
   container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
@@ -7,4 +7,5 @@ document.getElementById("scroll-left").addEventListener("click", () => {
 
 document.getElementById("scroll-right").addEventListener("click", () => {
   container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-});
+});   
+   
